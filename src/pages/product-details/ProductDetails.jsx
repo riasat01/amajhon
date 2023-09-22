255271bef35a8baa7daf41cb56a5be136e31a02f
unknown <riasat01@outlook.com>
2023-09-22 06:21:58 +0600

    navigation issue resolved

--- src/pages/product-details/ProductDetails.jsx
+++ src/pages/product-details/ProductDetails.jsx
@@ -22,7 +22,22 @@ const ProductDetails = () => {
     const navigation = useNavigation();
     console.log(navigation);
     return (
-        <section className="bg-slate-900 max-w-lg mx-auto my-12 rounded-xl shadow-xl">
+        // <section className="bg-slate-900 max-w-lg mx-auto my-12 rounded-xl shadow-xl">
+        //     <img className="rounded-xl mx-auto" src={thumbnail} alt={description} />
+        //     <div className="p-6">
+        //     <h1 className="text-3xl font-bold">{title}</h1>
+        //     <p>{description}</p>
+        //     <section className="flex justify-between items-center text-xl font-semibold mt-6">
+        //         <p className="">${price}</p>
+        //         <button className="bg-purple-700 px-6 py-2 rounded-xl">Add to Cart</button>
+        //     </section>
+        //     <button onClick={handleBack} className="w-full bg-purple-700 px-6 py-2 rounded-xl">Back</button>
+        //     </div>
+        // </section>
+
+       <div>
+         {
+            navigation.state === "loading" ? <span className="loading loading-infinity loading-lg"></span> : <section className="bg-slate-900 max-w-lg mx-auto my-12 rounded-xl shadow-xl">
             <img className="rounded-xl mx-auto" src={thumbnail} alt={description} />
             <div className="p-6">
             <h1 className="text-3xl font-bold">{title}</h1>
@@ -34,6 +49,8 @@ const ProductDetails = () => {
             <button onClick={handleBack} className="w-full bg-purple-700 px-6 py-2 rounded-xl">Back</button>
             </div>
         </section>
+        }
+       </div>
     );
 };
 
--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigation } from "react-router-dom";
 import Product from "../product/Product";
 
 const Products = () => {
@@ -6,13 +6,21 @@ const Products = () => {
     const products = useLoaderData();
     const items = products.products;
     console.log(items);
+    const navigation = useNavigation();
     return (
         <div>
-            <div className="grid grid-cols-4 gap-6 my-16">
+            {/* <div className="grid grid-cols-4 gap-6 my-16">
             {
                 items?.map(product  => <Product key={product?.id} product={product}></Product>)
             }
-            </div>
+            </div> */}
+            {
+                navigation.state === "loading" ? <span className="text-5xl loading loading-infinity loading-lg"></span> : <div className="grid grid-cols-4 gap-6 my-16">
+                {
+                    items?.map(product  => <Product key={product?.id} product={product}></Product>)
+                }
+                </div>
+            }
         </div>
     );
 };
